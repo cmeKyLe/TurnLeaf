fac239ba625543268576a288500158a25b3d5a7b
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-10-17 16:58:47 -0700

    added designs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#2081C3',
+        'secondary' : '#06070E',
+        'bg' : '#F3F3F3',
+        'Favorite' : '#FE5F55',
+      },
+      fontFamily: {
+        'primary' : ["Montserrat", "sans-serif"],
+        'secondary' : ["Nunito Sans", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 }

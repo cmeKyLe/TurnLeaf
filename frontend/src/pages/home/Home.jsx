086e34ad64525044e0ba7b910e453969944eae10
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-10-23 19:40:41 -0700

    finished recommedations section (need to work on backend for proper recommendations)

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommended from './Recommended'
 
 const Home = () => {
   return (
     <>
         <Banner/>
         <TopSellers/>
+        <Recommended/>
     </>
   )
 }
